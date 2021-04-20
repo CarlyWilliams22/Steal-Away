4fd5337714418b25d8edb0435bcde9cb2a68cf5a
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-04-20 16:43:57 -0400

    Character is now animated

--- Assets/Scripts/Player_MMovementScript.cs
+++ Assets/Scripts/Player_MMovementScript.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class Player_MMovementScript : MonoBehaviour
 {
     CharacterController controller;
-    public float SPEED = 1.5f;
+    public float SPEED = 3f;
     bool sneak = false;
     Vector3 normal;
     Vector3 shrink;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
         normal = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
         shrink = new Vector3(transform.localScale.x, transform.localScale.y*.5f, transform.localScale.z);
     }
@@ -29,8 +31,6 @@ public class Player_MMovementScript : MonoBehaviour
 
         controller.Move(moveVec);
 
-        controller.Move(moveVec);
-
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             sneak = !sneak;
@@ -39,15 +39,25 @@ public class Player_MMovementScript : MonoBehaviour
                 transform.localScale = shrink;
                 SPEED = .5f;
                 controller.center = new Vector3(0, 1.2f, 0);
+                animator.SetBool("Crouching", true);
             }
             else
             {
                 transform.localScale = normal;
-                SPEED = 1.5f;
+                SPEED = 3;
                 controller.center = new Vector3(0, .95f, 0);
+                animator.SetBool("Crouching", false);
             }
         }
         
+        if(controller.velocity == Vector3.zero)
+        {
+            animator.SetBool("Moving", false);
+        }
+        else
+        {
+            animator.SetBool("Moving", true);
+        }
 
     }
 }
