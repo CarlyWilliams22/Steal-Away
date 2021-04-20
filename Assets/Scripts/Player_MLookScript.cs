a9463896e53f0c0a3f0b7c8d718b77d55c9ff295
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-04-20 10:47:09 -0400

    Added Player prefab that can move and look around

---
+++ Assets/Scripts/Player_MLookScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_MLookScript : MonoBehaviour
+{
+
+    float yAngle = 0;
+    public GameObject head;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAngle = transform.eulerAngles.y + 5 * Input.GetAxis("Mouse X");
+        yAngle -= 5 * Input.GetAxis("Mouse Y");
+        yAngle = Mathf.Clamp(yAngle, -25, 50);
+
+        transform.eulerAngles = new Vector3(0, xAngle, 0);
+        head.transform.localEulerAngles = new Vector3(yAngle, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Player_MMovementScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_MMovementScript : MonoBehaviour
+{
+    CharacterController controller;
+    public float SPEED = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 moveVec = transform.rotation
+         * (Time.deltaTime * new Vector3(SPEED * Input.GetAxis("Horizontal"), 0,
+            SPEED * Input.GetAxis("Vertical")));
+
+        controller.Move(moveVec);
+
+        controller.Move(moveVec);
+
+    }
+}
