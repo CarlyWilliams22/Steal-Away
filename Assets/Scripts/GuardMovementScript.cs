ddd08373f309c6e76c9caef8474f7b191a7bc8f9
dreigle <70277016+dreigle@users.noreply.github.com>
2021-04-20 22:58:02 -0400

    Beginnings of the Guard

---
+++ Assets/Scripts/GuardMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardMovementScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+    public GameObject[] patrolCheckpoints;
+    int nextDestIndex;
+    Vector3 nextDest;
+    bool patrolling, pursuing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        patrolling = true;
+        nextDestIndex = 0;
+        nextDest = patrolCheckpoints[nextDestIndex].transform.position;
+        agent.SetDestination(nextDest);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((transform.position - nextDest).magnitude < .5)
+        {
+            nextDestIndex = (nextDestIndex + 1) % patrolCheckpoints.Length;
+            nextDest = patrolCheckpoints[nextDestIndex].transform.position;
+            agent.SetDestination(nextDest);
+        }
+
+
+        animator.SetBool("Patrolling", patrolling);
+        animator.SetBool("Pursuing", pursuing);
+    }
+}
