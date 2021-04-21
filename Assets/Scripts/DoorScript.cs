a57ec5ee64bcab79edfcf649d9f3c6f2b4556d6a
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-04-20 22:01:04 -0400

    add door animation

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator animator;
+
+    private const string ANIMATOR_PROPERTY_IS_OPEN = "isOpen";
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public bool isOpen
+    {
+        get => animator.GetBool(ANIMATOR_PROPERTY_IS_OPEN);
+        set => animator.SetBool(ANIMATOR_PROPERTY_IS_OPEN, value);
+    }
+}
