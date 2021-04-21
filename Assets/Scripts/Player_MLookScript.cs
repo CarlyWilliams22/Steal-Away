ea27584f4e839c2ab7ac7fb509cd06da36958464
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-04-20 20:20:27 -0400

    Fixed camera clipping issue

--- Assets/Scripts/Player_MLookScript.cs
+++ Assets/Scripts/Player_MLookScript.cs
@@ -19,7 +19,7 @@ public class Player_MLookScript : MonoBehaviour
     {
         float xAngle = transform.eulerAngles.y + 5 * Input.GetAxis("Mouse X");
         yAngle -= 5 * Input.GetAxis("Mouse Y");
-        yAngle = Mathf.Clamp(yAngle, -25, 50);
+        yAngle = Mathf.Clamp(yAngle, -25, 32.75f);
 
         transform.eulerAngles = new Vector3(0, xAngle, 0);
         head.transform.localEulerAngles = new Vector3(yAngle, 0, 0);
