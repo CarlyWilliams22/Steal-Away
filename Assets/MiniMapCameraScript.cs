ed8220113c22717923a74123cadc1808c8918a21
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-04-22 11:21:03 -0400

    implement security cameras

---
+++ Assets/MiniMapCameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraScript : MonoBehaviour
+{
+    float shadowDistance;
+    private void OnPreRender()
+    {
+        shadowDistance = QualitySettings.shadowDistance;
+        QualitySettings.shadowDistance = 0;
+    }
+
+    private void OnPostRender()
+    {
+        QualitySettings.shadowDistance = shadowDistance;
+    }
+}
