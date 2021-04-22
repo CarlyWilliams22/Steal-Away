1a67b6f17d39f36e3232a0132bccebbca78e1567
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-04-22 15:24:28 -0400

    improve camera performance

---
+++ Assets/Scripts/CameraManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManagerScript : MonoBehaviour
+{
+    public GameObject[] vanCameras;
+    private bool _enabled = false;
+
+    private void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.SWITCH_PLAYER, Switch);
+    }
+
+    private void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.SWITCH_PLAYER, Switch);
+    }
+
+    private void Switch()
+    {
+        _enabled = !_enabled;
+        foreach (GameObject cam in vanCameras)
+        {
+            cam.SetActive(_enabled);
+        }
+    }
+}
