d9d3114820fd7008d60d1e1b7d3c8cc5181020a1
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-04-23 01:27:12 -0400

    control doors from van

---
+++ Assets/Scripts/DoorManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManagerScript : MonoBehaviour
+{
+    private static DoorManagerScript _instance = null;
+    private DoorScript[] doors;
+
+    public static DoorManagerScript Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doors = FindObjectsOfType<DoorScript>();
+    }
+
+    public DoorScript ClosestDoorToPoint(Vector3 point)
+    {
+        DoorScript current = doors[0];
+        foreach (DoorScript door in doors)
+        {
+            if (Vector3.Distance(door.transform.position, point) < Vector3.Distance(current.transform.position, point))
+            {
+                current = door;
+            }
+        }
+        return current;
+    }
+
+}
--- Assets/Scripts/Player_VLookScript.cs
+++ Assets/Scripts/Player_VLookScript.cs
@@ -8,11 +8,16 @@ public class Player_VLookScript : MonoBehaviour
     public GameObject head;
     bool currCharacter = false;
     public GameObject _camera;
+    public GameObject playerM;
+    private Camera cam;
+    public Camera miniMapCamera;
+    public LayerMask mapScreenLayer;
+    public LayerMask floorLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        cam = _camera.GetComponent<Camera>();
     }
 
     private void OnEnable()
@@ -37,14 +42,49 @@ public class Player_VLookScript : MonoBehaviour
             transform.eulerAngles = new Vector3(0, xAngle, 0);
             head.transform.localEulerAngles = new Vector3(yAngle, 0, 0);
 
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit monitorHit;
+                if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out monitorHit, Mathf.Infinity, mapScreenLayer))
+                {
+                    Vector3 monitorPoint = monitorHit.collider.transform.InverseTransformPoint(monitorHit.point);
+
+                    // scale
+                    monitorPoint.x *= monitorHit.collider.transform.localScale.x;
+                    monitorPoint.y *= monitorHit.collider.transform.localScale.y;
+                    monitorPoint.z *= monitorHit.collider.transform.localScale.z;
+
+                    // convert to 0..1, for some reason 0.8 is magic
+                    const float range = 0.8f;
+                    monitorPoint = new Vector3(ConvertRange(monitorPoint.x, -range, range, 0, 1), ConvertRange(monitorPoint.y, -range, range, 0, 1), ConvertRange(monitorPoint.z, -range, range, 0, 1));
+
+                    // flip axis
+                    monitorPoint.x = 1 - monitorPoint.x;
+                    monitorPoint.z = 1 - monitorPoint.z;
+
+                    // raycast into the game world
+                    RaycastHit worldHit;
+                    if (Physics.Raycast(miniMapCamera.ViewportPointToRay(new Vector3(monitorPoint.x, monitorPoint.z, 0)), out worldHit, Mathf.Infinity, floorLayer))
+                    {
+                        // open the closest door
+                        DoorScript door = DoorManagerScript.Instance.ClosestDoorToPoint(worldHit.point);
+                        door.isOpen = !door.isOpen;
+                    }
+                }
+            }
         }
-
-
     }
 
     private void Switch()
     {
         currCharacter = !currCharacter;
         _camera.SetActive(currCharacter);
+    }
+
+    // https://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
+    private static float ConvertRange(float OldValue, float OldMin, float OldMax, float NewMin, float NewMax)
+    {
+        return (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin;
     }
 }
