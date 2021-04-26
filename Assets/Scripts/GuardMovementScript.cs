db89225954754c11251107fa42d10f8dfc3a2f6c
dreigle <70277016+dreigle@users.noreply.github.com>
2021-04-26 00:00:43 -0400

    Mostly guard opening and closing doors

--- Assets/Scripts/GuardMovementScript.cs
+++ Assets/Scripts/GuardMovementScript.cs
@@ -13,7 +13,8 @@ public class GuardMovementScript : MonoBehaviour
     public LayerMask guardLayer;
     int nextDestIndex;
     Vector3 nextDest;
-    bool patrolling, pursuing;
+    bool patrolling, pursuing, doorNext;
+    DoorScript door;
 
     // Start is called before the first frame update
     void Start()
@@ -29,24 +30,39 @@ public class GuardMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ((transform.position - nextDest).magnitude < .25)
+        if (patrolling)
         {
-            nextDestIndex = (nextDestIndex + 1) % patrolCheckpoints.Length;
-            nextDest = patrolCheckpoints[nextDestIndex].transform.position;
-            agent.SetDestination(nextDest);
+            if ((transform.position - nextDest).magnitude < .25)
+            {
+                SetNextPatrolDest();
+            }
+
+            if (doorNext)
+            {
+                if ((transform.position - nextDest).magnitude < 1)
+                {
+                    agent.ResetPath();
+                    patrolling = false;
+                    doorNext = false;
+                    door.isOpen = true;
+                    StartCoroutine(GoThroughDoor());
+                }
+            }
         }
 
         RaycastHit hit;
-        if (Physics.Raycast(head.transform.position, Vector3.forward, Mathf.Infinity, guardLayer))
+        if (Physics.SphereCast(head.transform.position, 1, head.transform.forward, out hit, Mathf.Infinity, guardLayer))
         {
-          //  print("hit");
-         //   if (hit.collider.gameObject.layer.Equals("PlayerM"))
+            //print("hit");
+            //   if (hit.collider.gameObject.layer.Equals("PlayerM"))
             {
-                
+
                 pursuing = true;
+                patrolling = false;
+                doorNext = false;
             }
         }
-      
+
         if (pursuing)
         {
             agent.SetDestination(player.transform.position);
@@ -54,5 +70,29 @@ public class GuardMovementScript : MonoBehaviour
 
         animator.SetBool("Patrolling", patrolling);
         animator.SetBool("Pursuing", pursuing);
+    }
+
+    IEnumerator GoThroughDoor()
+    {
+        yield return new WaitForSeconds(.2f);
+        
+        patrolling = true;
+        SetNextPatrolDest();
+
+        yield return new WaitForSeconds(2f);
+        door.isOpen = false;
+        print("Close");
+    }
+
+    private void SetNextPatrolDest()
+    {
+        nextDestIndex = (nextDestIndex + 1) % patrolCheckpoints.Length;
+        nextDest = patrolCheckpoints[nextDestIndex].transform.position;
+        agent.SetDestination(nextDest);
+        door = DoorManagerScript.Instance.ClosestDoorToPoint(nextDest);
+        if (door.transform.position == nextDest)
+        {
+            doorNext = true;
+        }
     }
 }
