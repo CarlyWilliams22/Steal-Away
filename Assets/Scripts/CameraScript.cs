292d11de1bf1e81e5cb2cda40be0337a2df3c22b
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-05-06 23:33:12 -0400

    different ambient light per camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private static readonly Color museamPlayerColor = new Color(25f / 255f, 25f / 255f, 25f / 255f);
+    private static readonly Color vanPlayerColor = new Color(0.5f, 0.5f, 0.5f);
+    private static readonly Color securityColor = new Color(200f / 255f, 200f / 255f, 200f / 255f);
+    private static readonly Color minimapColor = new Color(1, 1, 1);
+
+    public enum CameraType { Security, VanPlayer, MuseamPlayer, MiniMap };
+    public CameraType type;
+
+    private Color originalAmbient;
+
+    private void OnPreCull()
+    {
+        originalAmbient = RenderSettings.ambientLight;
+        switch (type)
+        {
+            case CameraType.Security:
+                RenderSettings.ambientLight = securityColor;
+                break;
+            case CameraType.MiniMap:
+                RenderSettings.ambientLight = minimapColor;
+                break;
+            case CameraType.VanPlayer:
+                RenderSettings.ambientLight = vanPlayerColor;
+                break;
+            case CameraType.MuseamPlayer:
+                RenderSettings.ambientLight = museamPlayerColor;
+                break;
+        }
+    }
+
+    private void OnPostRender()
+    {
+        RenderSettings.ambientLight = originalAmbient;
+    }
+}
