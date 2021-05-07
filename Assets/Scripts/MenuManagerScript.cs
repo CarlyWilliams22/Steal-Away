dd525bc576e171f4cebe4b1f5170dcce8175da4c
dreigle <70277016+dreigle@users.noreply.github.com>
2021-05-07 15:55:06 -0400

    Menu improvements, hud icon improvements

---
+++ Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    public GameObject menu, instructions;
+
+
+    public void OnClickInstructions()
+    {
+        menu.SetActive(false);
+        instructions.SetActive(true);
+    }
+
+    public void OnClickMenu()
+    {
+        menu.SetActive(true);
+        instructions.SetActive(false);
+    }
+}
