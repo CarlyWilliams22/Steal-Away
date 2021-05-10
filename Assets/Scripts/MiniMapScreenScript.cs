88296b36a3005c89f5df17327ba42cf298b10191
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-05-10 17:08:45 -0400

    mini map screen goes red on alarm

---
+++ Assets/Scripts/MiniMapScreenScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class MiniMapScreenScript : MonoBehaviour
+{
+    private ColorGrading colorGrading;
+    private PostProcessVolume postProcessVolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        postProcessVolume = GetComponent<PostProcessVolume>();
+        postProcessVolume.profile.TryGetSettings(out colorGrading);
+    }
+
+    private void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.ALARM_SOUNDED, OnAlarmSounded);
+    }
+
+    private void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.ALARM_SOUNDED, OnAlarmSounded);
+    }
+
+    private void OnAlarmSounded()
+    {
+        colorGrading.colorFilter.value = Color.red;
+    }
+}
