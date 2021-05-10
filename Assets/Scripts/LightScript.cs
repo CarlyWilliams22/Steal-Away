6abd22f34b1eadb2a0def3179e04f2b092203a30
CarlyWilliams22 <57012119+CarlyWilliams22@users.noreply.github.com>
2021-05-09 23:16:43 -0400

    Lights now turn red when alarm goes off

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    Light _light;
+
+    void Start()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.ALARM_SOUNDED, SwitchColor);
+    }
+
+    private void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.ALARM_SOUNDED, SwitchColor);
+    }
+
+    void SwitchColor()
+    {
+        _light.color = Color.red;
+    }
+
+}
