fb65dfcdacb5e6d2ed57bdf1ed63f654cedf4e2d
Nathan Fulton <7968460+N104@users.noreply.github.com>
2021-05-10 17:17:22 -0400

    all monitors turn red when alarm goes off

--- Assets/Scripts/MonitorFXScript.cs
+++ Assets/Scripts/MonitorFXScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
-public class MiniMapScreenScript : MonoBehaviour
+public class MonitorFXScript : MonoBehaviour
 {
     private ColorGrading colorGrading;
     private PostProcessVolume postProcessVolume;
