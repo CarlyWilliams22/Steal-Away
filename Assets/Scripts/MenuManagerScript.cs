7000c9d07c4fbc478c9a34918c01a92d24331929
dreigle <70277016+dreigle@users.noreply.github.com>
2021-05-10 20:42:43 -0400

    Best time on the main menu

--- Assets/Scripts/MenuManagerScript.cs
+++ Assets/Scripts/MenuManagerScript.cs
@@ -1,10 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Assets.Scripts;
 
 public class MenuManagerScript : MonoBehaviour
 {
     public GameObject menu, instructions;
+    public Text highScoreText;
+
+    private void Start()
+    {
+        if (Prefs.IsDefaultValue(Prefs.Property.Highscore))
+        {
+            highScoreText.text = "";
+        }
+        else
+        { 
+            highScoreText.text = "Best time: " + System.TimeSpan.FromSeconds(Prefs.GetFloat(Prefs.Property.Highscore)).ToString("mm':'ss");
+        }
+
+    }
 
 
     public void OnClickInstructions()
